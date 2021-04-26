acd9fc083719648dd8d602438b28b3c4edce5f7d
Saksham Jain <saksham.jain@sap.com>
2021-04-26 23:23:12 +0530

    Angular Playground
    App to Learn Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './myComponent/home/home.component';
+import { ListComponent } from './myComponent/list/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component:HomeComponent },
+  { path: 'list', component:ListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './myComponent/home/home.component';
+import { ListComponent } from './myComponent/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
